79aa57406e61a73b92451e3142a291f4bfad1998
whispermy <49235958+whispermy@users.noreply.github.com>
2021-04-23 06:58:01 +0900

    BFS 4번 문제 해결, 해시 2번 문제 진행중

--- programmers/clothes.py
+++ programmers/clothes.py
@@ -1,11 +1,24 @@
 def solution(clothes):
     answer = 0
-    diction = {}
-    k = 0
-    for i in clothes
+    keys = clothes[1][1]
+    number = 0
+    line = []
+    mul = 1
         
+    while clothes:
+        for i in clothes:
+            if i[1] == keys:
+                number += 1
+                clothes.remove(i)
+
+        line.append(number)
+        number = 0
+        
+    for j in line:
+        answer += j
+        mul *= j
+        
+    return answer+mul
 
 
-
-    return answer
-
+print(solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]))
--- programmers/network.py
+++ programmers/network.py
@@ -3,24 +3,54 @@
 #     for l in (0,n):
                 
                 
-#     return answer
-answer = 0
-index = []
-n = 8
+# #     return answer
+# answer = 0
+# index = []
+# n = 8
 
-for i in range (0,n):
-    index.append(i)
+# for i in range (0,n):
+#     index.append(i)
     
-for i in range (0,n):
-    for j in range (0,n):
-        if computers[i][j] == 1:
-            if index[i] == i:
-                index[i] = 0 
-            if index[j] == j:
-                index[j] = 0
+# for i in range (0,n):
+#     for j in range (0,n):
+#         if computers[i][j] == 1:
+#             if index[i] == i:
+#                 index[i] = 0 
+#             if index[j] == j:
+#                 index[j] = 0
     
-    for k in range (0,n):
-        if index[k] != 0:
-            i = index[k]
+#     for k in range (0,n):
+#         if index[k] != 0:
+#             i = index[k]
+#             answer += 1
+#             break
+
+from collections import deque
+
+def solution(n, computers):
+    answer = 0
+    index = [False]*(n+1)
+    # queue = deque([1])
+    queue = []
+        
+    for i in range (1,n+1):
+        if index[i] == False:
+            queue = deque([i])
+            index[i] =  True
+            
+            while queue:
+                v = queue.popleft()
+
+                c = computers[v-1]
+                for k in range (0,len(c)):
+                    if c[k] == 1:
+                        if not index[k+1]:
+                            queue.append(k+1)
+                            index[k+1] = True
+              
             answer += 1
-            break
+            
+    
+    return answer
+
+print(solution(3,[[1,1,0],[1,1,1],[0,1,1]]))
