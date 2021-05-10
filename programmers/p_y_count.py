9dbbc44ca3362c1920acff6c81e0df900a030c34
whispermy <49235958+whispermy@users.noreply.github.com>
2021-05-11 00:10:43 +0900

    프로그래머스 문제 풀이 1문제 추가

---
+++ programmers/p_y_count.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = True
+    pcount = 0
+    ycount = 0
+    
+    for i in s:
+        if i == 'p' or i == 'P':
+            pcount += 1
+        elif i == 'y' or i == 'Y':
+            ycount += 1
+
+    if pcount == ycount:
+        return True
+    else:
+        return False
